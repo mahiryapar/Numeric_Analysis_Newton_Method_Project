5d544a38a39680a6f1ae0d454b2b42cd0797ed1c
Mahir Yapar <mahiryapar2453@gmail.com>
2024-03-28 23:20:07 +0300

    Klavyeden fonksiyon girişi sağlandı.

--- Newton_Method.py
+++ Newton_Method.py
@@ -1,15 +1,27 @@
 import math
-from sympy import symbols, diff
+from sympy import symbols,sympify, diff
 
 x = symbols("x")
 y = symbols("y")
-e = math.e
+e = symbols("e")
 pi = math.pi
 inf = math.inf
-
-fx = x**2 + 3*x + 2
-
 
 def turev(fx):
     fxt = diff(fx,x)
     return fxt
+
+def bilgi_al():
+    ctrl = True
+    while ctrl == True:
+        ctrl = False
+        fx = input("Newton Metodunu uygulamak istediğiniz fonksiyonu giriniz -> ")
+        try:
+            fx = sympify(fx)
+        except:
+            print("Fonksiyon girdisinde hata! Lütfen tekrar deneyiniz.")
+            ctrl = True
+            continue
+        ctrl = False
+        return fx
+
