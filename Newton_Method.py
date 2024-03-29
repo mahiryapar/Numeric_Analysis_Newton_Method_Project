03119174733ed743457449a8a6cdbf61739e678d
CumaaB <cumabalci2003@gmail.com>
2024-03-29 12:53:20 +0300

    bilgi_al() fonksiyonunda hata seviyesi ve aralık değerleri bilgileri alındı.

--- Newton_Method.py
+++ Newton_Method.py
@@ -8,7 +8,7 @@ pi = math.pi
 inf = math.inf
 
 def turev(fx):
-    fxt = diff(fx,x)
+    fxt = diff(fx, x)
     return fxt
 
 def bilgi_al():
@@ -20,5 +20,31 @@ def bilgi_al():
             print("Fonksiyon girdisinde hata! Lütfen tekrar deneyiniz.")
             ctrl = True
             continue
-        return fx
 
+        while True:
+            try:
+                error_level = float(input("Hata seviyesi giriniz (Örneğin:0.001)->"))
+                break
+            except ValueError:
+                print("Lütfen gecerli bir değer giriniz!")
+                continue
+        while True:
+            try:
+                print("Kök bulmak istediğiniz bir aralık belirleyiniz.")
+                range_start_value = float(input("Aralığın başlangıç değerini giriniz ->"))
+                range_finish_value = float(input("Aralığın bitiş değerini giriniz ->"))
+                break
+            except ValueError:
+                print("Lütfen gecerli aralık değerleri giriniz!")
+                continue
+
+    return fx ,error_level, range_start_value, range_finish_value
+
+
+
+def print_yaz():
+    fx, error_level, range_start_value, range_finish_value = bilgi_al()
+    print(f"Error level :{error_level}, Aralık başlangıcı :{range_start_value}, Aralık bitişi :{range_finish_value}")
+    print(turev(fx))
+
+print_yaz()
