006348258e7141a21ec16a64a9fc4a4159433ec0
CumaaB <cumabalci2003@gmail.com>
2024-03-29 13:47:12 +0300

    new_ton_method fonksiyonu yazıldı.

--- Newton_Method.py
+++ Newton_Method.py
@@ -9,8 +9,6 @@ def turev(fx):
     fxt = diff(fx, x)
     return fxt
 
-
-
 def bilgi_al():
     while True:
         fx = input("Newton Metodunu uygulamak istediğiniz fonksiyonu giriniz -> ")
@@ -39,19 +37,27 @@ def bilgi_al():
     return fx ,error_level, range_start_value, range_finish_value
 
 
-
 def newton_method(fx, error_level, range_start_value, range_finish_value):
-    p0 = float(range_start_value+range_finish_value)/2 # p0 baslangic değerimiz.
+    p_0 = float(range_start_value+range_finish_value)/2 # p0 baslangic değerimiz.
+    current_iter = 0
+    max_iter =100
+    while(current_iter < max_iter):
+        current_iter +=1
+        p_n = p_0 -fx.subs(x, p_0) / diff(fx, x)
+        if(p_n < error_level):
+            return p_n
+        p_0 = p_n
 
-
+    if current_iter < max_iter:
+        print(f"Kök bulundu: {p_n} (iterasyon sayısı: {current_iter})")
+    else:
+        print("Maksimum iterasyon sayısına ulaşıldı. Kök bulunamadı.")
 
 def main():
     fx, error_level, range_start_value, range_finish_value = bilgi_al()
     print(f"Error level :{error_level}, Aralık başlangıcı :{range_start_value}, Aralık bitişi :{range_finish_value}")
     print(turev(fx))
-    newton_method(fx, error_level, range_start_value, range_finish_value)
-
-
+    root = newton_method(fx, error_level, range_start_value, range_finish_value)
 
 
 if __name__ == "__main__":        #programın çalışmasını sağlıyor
