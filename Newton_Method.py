4960bad9351bfa90ddeadfd07f14a1feb52cc58d
Mahir Yapar <mahiryapar2453@gmail.com>
2024-03-29 13:02:39 +0300

    bilgi_al() fonksiyonundaki sonsuz döngü bug'ı giderildi.

--- Newton_Method.py
+++ Newton_Method.py
@@ -11,34 +11,34 @@ def turev(fx):
     fxt = diff(fx, x)
     return fxt
 
-def bilgi_al():
-    while True:
-        fx = input("Newton Metodunu uygulamak istediğiniz fonksiyonu giriniz -> ")
-        try:
-            fx = sympify(fx)
-        except:
-            print("Fonksiyon girdisinde hata! Lütfen tekrar deneyiniz.")
-            ctrl = True
-            continue
 
+
+def bilgi_al():
+        while True:
+            fx = input("Newton Metodunu uygulamak istediğiniz fonksiyonu giriniz -> ")
+            try:
+                fx = sympify(fx)
+            except:
+                 print("Fonksiyon girdisinde hata! Lütfen tekrar deneyiniz.")
+                 continue
+            break
         while True:
             try:
                 error_level = float(input("Hata seviyesi giriniz (Örneğin:0.001)->"))
-                break
             except ValueError:
                 print("Lütfen gecerli bir değer giriniz!")
                 continue
+            break
         while True:
             try:
                 print("Kök bulmak istediğiniz bir aralık belirleyiniz.")
                 range_start_value = float(input("Aralığın başlangıç değerini giriniz ->"))
                 range_finish_value = float(input("Aralığın bitiş değerini giriniz ->"))
-                break
             except ValueError:
                 print("Lütfen gecerli aralık değerleri giriniz!")
                 continue
-
-    return fx ,error_level, range_start_value, range_finish_value
+            break
+        return fx ,error_level, range_start_value, range_finish_value
 
 
 
